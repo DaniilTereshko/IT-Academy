6e8ef72fc90ce608c9ee388e921ba42d32e6857f
DaniilTereshko <daniiltereshko123@gmail.com>
2022-11-02 08:43:10 +0300

    homework

--- src/exam/Grade4.java
+++ src/exam/Grade4.java
@@ -1,31 +1,16 @@
 package exam;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Scanner;
-import java.util.regex.Pattern;
+import java.util.Set;
 
 public class Grade4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
-
-        Pattern pattern = Pattern.compile("\\W+");
-        String[] words = pattern.split(input.trim());
-
-        int countUnique = 0;
-        boolean isUnq = true;
-        for (int i = 0; i < words.length; i++) {
-            for (int j = 0; j < words.length; j++) {
-                if(words[i].equalsIgnoreCase(words[j])&& (i != j)){
-                    isUnq = false;
-                    break;
-                }
-            }
-            if(isUnq){
-                countUnique++;
-            }
-            isUnq = true;
-        }
-        System.out.println("Number of unique words: " +countUnique);
+        String[] split = input.toLowerCase().trim().split("\\W+");
+        Set uniqueWords = new HashSet(Arrays.asList(split));
+        System.out.println(uniqueWords.size());
     }
-
 }
