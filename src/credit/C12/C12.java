2545f78a9c0e7192ef14ec2240fb9e7b27d87107
DaniilTereshko <daniiltereshko123@gmail.com>
2022-11-07 13:57:03 +0300

    homework

--- src/credit/C12/C12.java
+++ src/credit/C12/C12.java
@@ -5,11 +5,11 @@ import java.util.Random;
 
 public class C12 {
     static final String FILENAME = "src\\credit\\C12\\C12.bin";
-    static final int nOfNumbers = 30;
+    static final int N_OF_NUMBERS = 30;
     public static void main(String[] args) {
         float sum = 0F;
         try(var fis = new FileOutputStream(FILENAME); var bis = new BufferedInputStream(new FileInputStream(FILENAME))) {
-            for (int i = 0; i < nOfNumbers; i++) {
+            for (int i = 0; i < N_OF_NUMBERS; i++) {
                 int number = (int)(Math.random()*100)+1;
                 fis.write(number);
             }
@@ -18,7 +18,7 @@ public class C12 {
                 sum+=number;
                 System.out.print(number+ " ");
             }
-            System.out.println("\nСреднее арифметическое: "+ sum/nOfNumbers);
+            System.out.println("\nСреднее арифметическое: "+ sum/N_OF_NUMBERS);
 
         } catch (IOException e) {
             e.printStackTrace();
---
+++ src/exam/Grade8/Grade8.java
@@ -0,0 +1,41 @@
+package exam.Grade8;
+
+import java.io.*;
+import java.util.*;
+
+public class Grade8 {
+    static final int NUMBERS = 1000;
+    static final String IN1 = "src\\exam\\Grade8\\in1.txt";
+    static final String IN2 = "src\\exam\\Grade8\\in2.txt";
+    static final String OUT = "src\\exam\\Grade8\\out.txt";
+
+    public static void main(String[] args) {
+        try(var bw1 = new BufferedWriter(new FileWriter(IN1));
+            var bw2 = new BufferedWriter(new FileWriter(IN2))) {
+            int number1, number2;
+            for (int i = 0; i < NUMBERS; i++) {
+                number1 = (int)(Math.random()*100000)+1;
+                number2 = (int)(Math.random()*100000)+1;
+                bw1.write(number1 + " ");
+                bw2.write(number2 + " ");
+            }
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+        try(var scanner1 = new Scanner(new FileReader(IN1));
+            var scanner2 = new Scanner(new FileReader(IN2));
+            var bw = new BufferedWriter(new FileWriter(OUT))){
+            SortedSet<Integer> numbers = new TreeSet<>();
+            while(scanner1.hasNext() && scanner2.hasNext()){
+                numbers.add(scanner1.nextInt());
+                numbers.add(scanner2.nextInt());
+            }
+            bw.write(numbers.toString());
+            System.out.println(numbers);
+        }
+        catch (IOException ex){
+            ex.printStackTrace();
+        }
+    }
+}
